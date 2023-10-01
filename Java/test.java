bcc1db931c09af6f0461fc7be1b8c495f19c67a9
Doan Phuong Nam <22520908@gm.uit.edu.vn>
2023-10-01 22:18:46 +0700

    10/1/2023

--- Java/test.java
+++ Java/test.java
@@ -9,21 +9,21 @@ public class test {
     }
     public static void main(String[] args) {
         
-        // Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         
-        // int[][] sam = new int[10][10];
-        // for(int i = 0 ; i < 2 ; i++){
-        //     for(int j = 0 ; j < 2 ; j++){
-        //         sam[i][j] = sc.nextInt();
-        //     }
-        // }
-        // sc.close();
-        // for(int i = 0 ; i < 10 ; i++){
-        //     for(int j = 0 ; j < 10 ; j++){
-        //         System.out.print(sam[i][j] + " ");
-        //     }
+        int[][] sam = new int[10][10];
+        for(int i = 0 ; i < 2 ; i++){
+            for(int j = 0 ; j < 2 ; j++){
+                sam[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+        for(int i = 0 ; i < 10 ; i++){
+            for(int j = 0 ; j < 10 ; j++){
+                System.out.print(sam[i][j] + " ");
+            }
             
-        //     System.out.println();
+            System.out.println();
 
             secondte mySecondte = new secondte();
 
@@ -34,3 +34,4 @@ public class test {
     
     }
 
+}
