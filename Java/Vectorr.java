1c64d17855a94bfd9b8170b4ef4712984938243f
Doan Phuong Nam <22520908@gm.uit.edu.vn>
2023-10-02 10:24:03 +0700

    Learn vector 2/10/2023

---
+++ Java/Vectorr.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Vectorr {
+    public static void main(String[] args) {
+        Vector<Integer> a = new Vector<Integer>();
+
+        a.add(0, 1);
+        System.out.println(a.get(0));
+    }
+}
