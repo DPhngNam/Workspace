f20aec190ac6ede5f253b28c22f622561fd9c947
Doan Phuong Nam <22520908@gm.uit.edu.vn>
2023-10-03 22:05:34 +0700

    Adding Set in java

--- Java/Sett.java
+++ Java/Sett.java
@@ -1,4 +1,14 @@
 import java.util.*;
+// there are 3 types of set : Tree set, hash set, linkedhash set
 public class Sett {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Set<Integer> s = new HashSet<>(n);
+        for(int i = 0 ; i < n;i++){
+            int temp = sc.nextInt();
+            s.add(temp);
+        }
+        s.forEach(System.out::println);
+    }
 }
