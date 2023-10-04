281035408798515810b9eaa407a1217ce29bd133
Doan Phuong Nam <22520908@gm.uit.edu.vn>
2023-10-04 17:22:20 +0700

    adjust LinkedHashSet to set.java

--- Java/Sett.java
+++ Java/Sett.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 // there are 3 types of set : Tree set, hash set, linkedhash set
 public class Sett {
     public static void main(String[] args){
@@ -10,5 +11,23 @@ public class Sett {
             s.add(temp);
         }
         s.forEach(System.out::println);
+        System.out.println();
+        Set<Integer> x = new LinkedHashSet<>(n);
+        for(int i = 0 ; i < n;i++){
+            int temp = sc.nextInt();
+            x.add(temp);
+        }
+        x.remove(4);
+        System.out.println(x.size());
+        x.forEach(System.out::println);
+        
+        System.out.println();
+        Set<Integer> t = new LinkedHashSet<>(n);
+        for(int i = 0 ; i < n;i++){
+            int temp = sc.nextInt();
+            t.add(temp);
+        }
+        
+        t.forEach(System.out::println);
     }
 }
