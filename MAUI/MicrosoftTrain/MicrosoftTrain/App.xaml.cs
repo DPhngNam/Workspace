16d3c52957006d26ef1e8d1340427533883d9b95
Đoàn Phương Nam <22520908@gm.uit.edu.vn>
2023-10-19 23:38:50 +0700

    Adding MAUI microsoft learn

---
+++ MAUI/MicrosoftTrain/MicrosoftTrain/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MicrosoftTrain
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MAUI/MicrosoftTrain/MicrosoftTrain/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MicrosoftTrain
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
